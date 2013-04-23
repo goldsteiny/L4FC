ab3f162adbf43c3e314ae2765b4f2aaf3177d3d1
Yaron Goldstein <Yaron@z7319.pia.fu-berlin.de>
2013-04-23 10:27:59 +0200

    Licenses added

--- LatticeModeling/Converter.cs
+++ LatticeModeling/Converter.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+ *  Copyright (C) 2013 Dr. Yaron Goldstein, Freie Universitaet Berlin
+ *  
+ *  This work is licensed under the Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0) License. 
+ *  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
+ *  or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ *  
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the License for more details. 
+ *  You should have received a copy of the License along with this program. 
+ *  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- LatticeModeling/Program.cs
+++ LatticeModeling/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+ *  Copyright (C) 2013 Dr. Yaron Goldstein, Freie Universitaet Berlin
+ *  
+ *  This work is licensed under the Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0) License. 
+ *  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ 
+ *  or send a letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ *  
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the License for more details. 
+ *  You should have received a copy of the License along with this program. 
+ *  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +25,139 @@ namespace FCA
 {
     public class FCA
     {
+
+
+        public static void Main(string[] args)
+        {
+
+            //init
+            string path2models = args[0], name, separator = args[1];
+
+            bool use_names = true;
+            bool do_efca = args.Length > 5 && int.Parse(args[5]) == 1, no_output = args.Length > 4 && int.Parse(args[4]) == 0;
+
+            int n_simulations = args.Length > 6 ? int.Parse(args[6]) : 1;
+            double max_value = double.Parse(args[2]), tolerance = double.Parse(args[3]);
+            LinkedList<bool[]> forbidden = new LinkedList<bool[]>();
+
+
+            double[,] matrix; bool[] rev;
+            for (int i = 0; i < 1; ++i)
+            {
+                bool fc = i  == 1;
+
+
+                LinkedList<string> paths = Reader.ReadFile(path2models), res = new LinkedList<string>(), details = new LinkedList<string>();
+                res.AddFirst("\\begin{table}");
+                res.AddLast("\\centering");
+                res.AddLast("\\begin{tabular}{llrrrr}");
+                res.AddLast("\\toprule");
+                res.AddLast("\\rowcolor{gray!10}\\textbf{Model}&\\textbf{Value}&\\textbf{Number}&\\textbf{LPs}&\\textbf{Witnesses}&\\textbf{Time}\\\\");
+                //res.AddLast("Model Method Metabolites Reactions Reactions.unblocked Reactions.frev Couples Time Time.unblocked Time.frev Time.couples Time.efca LPs LPs.unblocked LPs.frev LPs.couples LPs.efca Witnesses Witnesses.unblocked Witnesses.frev Witnesses.couples Witnesses.efca");
+                //details.AddLast("Model Method Metabolites Reactions Reactions.unblocked Reactions.frev Couples Reaction Time LPs LPs.total Witnesses Witnesses.total Witnesses.distribution Witnesses.usable");
+                String model, method;
+                foreach (string path in paths)
+                {
+
+                    // read network
+                    forbidden.Clear();
+
+                    String[] metabolite_names = null, reaction_names = null;
+                    if (path.Contains("matrev"))
+                    {
+                        List<double>[] network = Reader.ReadDoubleFileToList(path, separator);
+                        NetworkReader.ExtractNetwork(network, out matrix, out rev);
+                        name = path.Substring(0, path.IndexOf(".matrev"));
+                    }
+                    else if (path.Contains("meta"))
+                    {
+                        NetworkReader.ReadMetatool(Reader.ReadFile(path), out matrix, out rev, out metabolite_names, out reaction_names);
+                        name = path.Substring(0, path.IndexOf(".meta"));
+                    }
+                    else
+                    {
+                        name = path;
+                        NetworkReader.ReadDir(name, separator, out matrix, out rev, out metabolite_names, out reaction_names, out forbidden);
+                    }
+                    Writer.WriteFile(NetworkReader.NetworkToDot(matrix, rev, metabolite_names, reaction_names), name + "_network.dot");
+                    Writer.WriteFile(NetworkReader.NetworkToConnections(matrix, rev, metabolite_names, reaction_names), name + "_connections.dot");
+                    Writer.WriteFile(NetworkReader.NetworkToMatlab(matrix, rev), name + ".m");
+
+                    model = name.Substring(name.LastIndexOf('\\') + 1);
+
+                    //bool[] trans = FindTrans(matrix, rev);
+
+                    // Calculations.
+
+                    LinkedList<String> lines = new LinkedList<string>();
+                    {
+                        FCA fca = null;
+                        method = fc ? "fc" : "all";
+                        for (int t = 0; t < n_simulations; ++t)
+                        {
+
+
+                            try
+                            {
+                                fca = new FCA(matrix, rev, forbidden, max_value, tolerance, fc, do_efca, !no_output);
+
+
+                                fca.Results(model, method, res, details);
+                                //Writer.WriteFile(details, String.Format("{0}_details_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
+                                Writer.WriteFile(res, String.Format("{0}_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
+                            }
+                            catch (CalculationException e)
+                            {
+                                Console.WriteLine("There's happened an error during the calculation. Repeating the simulation.");
+                                --t;
+                            }
+                        }
+
+                        // save results of last simulation
+
+                        String suffix = (fc ? "_fc" : "_all");
+
+                        lines.Clear();
+                        bool[] max = fca.max;
+                        int n = fca.n;
+                        for (int r = 0; r < n; ++r)
+                            if (!max[r])
+                                if (use_names)
+                                    lines.AddLast(reaction_names[r]);
+                                else
+                                    lines.AddLast(r.ToString());
+                        Writer.WriteFile(lines, name + "\\blocked" + suffix + ".txt");
+
+                        lines.Clear();
+                        for (int r = 0; r < n; ++r)
+                            if (max[r])
+                                for (int s = 0; s < n; ++s)
+                                    if (max[s] && !fca.coupling.Max(r)[s] && r != s)
+                                        if (use_names)
+                                            lines.AddLast(String.Format("{0} -> {1}", reaction_names[r], reaction_names[s]));
+                                        else
+                                            lines.AddLast(String.Format("{0} -> {1}", r, s));
+                        Writer.WriteFile(lines, name + "\\coupled" + suffix + ".txt");
+
+                        Writer.WriteFile(fca.coupling.ToDot(reaction_names), name + "\\coupled" + suffix + ".dot");
+
+                        if (do_efca)
+                            Writer.WriteFile(fca.efca.ToCSV(separator, reaction_names), name + "\\efca" + suffix + ".txt");
+
+                        Writer.WriteFile(res, String.Format("{0}_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
+                    }
+                }
+
+                // Finish stats.
+                res.AddLast("\\bottomrule");
+                res.AddLast("\\end{tabular}");
+                res.AddLast("\\end{table}");
+                Writer.WriteFile(res, String.Format("{0}_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
+
+            }
+            Console.WriteLine("Done. [press Enter]");
+            Console.ReadLine();
+        }
         
             private bool[] max;
             private IIntCoupling coupling; 
@@ -180,26 +327,7 @@ namespace FCA
                         res.AddLast(String.Format("\\cmidrule{4} & $\\Coupling$ &{0}&{2}&{3}&{1:0.0}\\\\", couples, (time_couples - time_unblocked).TotalSeconds, lps_couples, wits_couples, "{2-6}", cellcolor));
             if(efca!=null)
                 res.AddLast(String.Format("\\cmidrule{4} & {5}EFCA &{5}{1}&{5}{2}&{5}{3}&{5}{1:0.0}\\\\", efca.Length, (time_efca - time_couples).TotalSeconds, lps_efca, wits_efca, "{2-6}", cellcolor));
-              
-            /*res.AddLast(String.Format("{0} {1} {2} {3} {4} {5} {6} {7:0.000} {8:0.000} {9:0.000} {10:0.000} {11:0.000} {12} {13} {14} {15} {16} {17} {18} {19} {20} {21}", model, method, matrix.Length / rev.Length, rev.Length, reactions_unblocked, reactions_frev, couples, (time_efca - time_start).TotalSeconds, (time_unblocked - time_start).TotalSeconds, (time_frev - time_unblocked).TotalSeconds, (time_couples - time_frev).TotalSeconds, (time_efca - time_couples).TotalSeconds, lps, lps_unblocked, lps_frev, lps_couples, lps_efca, wits, wits_unblocked, wits_frev, wits_couples, wits_efca));
-
-            //unblocked
-            details.AddLast(String.Format("{0} {1} {2} {3} {4} {5} {6} {22:0.000} {23} {24} {25} {26} {27}", model, method, m, n, reactions_unblocked, reactions_frev, couples, (time_efca - time_start).TotalSeconds, (time_unblocked - time_start).TotalSeconds, (time_frev - time_unblocked).TotalSeconds, (time_couples - time_frev).TotalSeconds, (time_efca - time_couples).TotalSeconds, lps, lps_unblocked, lps_frev, lps_couples, lps_efca, wits, wits_unblocked, wits_frev, wits_couples, wits_efca, time_start, -1, lps_unblocked, lps_unblocked, wits_unblocked, wits_unblocked));
-            //couples
-            int lps_total = lps_unblocked;
-            int wits_total = wits_unblocked;
-            int temp_i = 0;
-            for (int i = 0; i < n; ++i)
-                if (max[i] && lps_reaction[i] > -1)
-                {
-                    lps_total += lps_reaction[i];
-                    wits_total += wits_reaction[i];
-                    temp_i = i;
-                    details.AddLast(String.Format("{0} {1} {2} {3} {4} {5} {6} {22:0.000} {23} {24} {25} {26} {27} {28} {29}", model, method, matrix.Length / rev.Length, rev.Length, reactions_unblocked, reactions_frev, couples, do_efca ? (time_efca - time_start).TotalSeconds : 0, (time_unblocked - time_start).TotalSeconds, (time_frev - time_unblocked).TotalSeconds, (time_couples - time_frev).TotalSeconds, do_efca ? (time_efca - time_couples).TotalSeconds : 0, lps, lps_unblocked, lps_frev, lps_couples, lps_efca, wits, wits_unblocked, wits_frev, wits_couples, wits_efca, i + 1, (time_reaction[i] - time_start).TotalSeconds, lps_reaction[i], lps_total, wits_reaction[i], wits_total, wits_distribution[i + 1], do_efca ? wits_usable[i] : 0));
-                }
-                else
-                    details.AddLast(String.Format("{0} {1} {2} {3} {4} {5} {6} {22:0.000} {23} {24} {25} {26} {27} {28} {29}", model, method, matrix.Length / rev.Length, rev.Length, reactions_unblocked, reactions_frev, couples, do_efca ? (time_efca - time_start).TotalSeconds : 0, (time_unblocked - time_start).TotalSeconds, (time_frev - time_unblocked).TotalSeconds, (time_couples - time_frev).TotalSeconds, do_efca ? (time_efca - time_couples).TotalSeconds : 0, lps, lps_unblocked, lps_frev, lps_couples, lps_efca, wits, wits_unblocked, wits_frev, wits_couples, wits_efca, i + 1, (time_reaction[temp_i] - time_start).TotalSeconds, 0, lps_total, 0, wits_total, wits_distribution[i + 1], do_efca ? wits_usable[i] : 0));
-        */
+            
         }
 
         public static bool[] FindTrans(double[,] matrix, bool[] rev)
@@ -222,137 +350,6 @@ namespace FCA
             return trans;
         }
 
-        public static void Main(string[] args)
-        {
-
-            //init
-            string path2models = args[0], name, separator = args[1];
-
-            bool use_names = true;
-            bool do_efca = args.Length > 5 && int.Parse(args[5]) == 1, no_output = args.Length > 4 && int.Parse(args[4]) == 0;
-
-            int n_simulations = args.Length > 6 ? int.Parse(args[6]) : 1;
-            double max_value = double.Parse(args[2]), tolerance = double.Parse(args[3]);
-            LinkedList<bool[]> forbidden = new LinkedList<bool[]>();
-
-
-            double[,] matrix; bool[] rev;
-            for (int i = 0; i < 1; ++i)
-            {
-                bool fc = i  == 1;
-
-
-                LinkedList<string> paths = Reader.ReadFile(path2models), res = new LinkedList<string>(), details = new LinkedList<string>();
-                res.AddFirst("\\begin{table}");
-                res.AddLast("\\centering");
-                res.AddLast("\\begin{tabular}{llrrrr}");
-                res.AddLast("\\toprule");
-                res.AddLast("\\rowcolor{gray!10}\\textbf{Model}&\\textbf{Value}&\\textbf{Number}&\\textbf{LPs}&\\textbf{Witnesses}&\\textbf{Time}\\\\");
-                //res.AddLast("Model Method Metabolites Reactions Reactions.unblocked Reactions.frev Couples Time Time.unblocked Time.frev Time.couples Time.efca LPs LPs.unblocked LPs.frev LPs.couples LPs.efca Witnesses Witnesses.unblocked Witnesses.frev Witnesses.couples Witnesses.efca");
-                //details.AddLast("Model Method Metabolites Reactions Reactions.unblocked Reactions.frev Couples Reaction Time LPs LPs.total Witnesses Witnesses.total Witnesses.distribution Witnesses.usable");
-                String model, method;
-                foreach (string path in paths)
-                {
-
-                    // read network
-                    forbidden.Clear();
-
-                    String[] metabolite_names = null, reaction_names = null;
-                    if (path.Contains("matrev"))
-                    {
-                        List<double>[] network = Reader.ReadDoubleFileToList(path, separator);
-                        NetworkReader.ExtractNetwork(network, out matrix, out rev);
-                        name = path.Substring(0, path.IndexOf(".matrev"));
-                    }
-                    else if (path.Contains("meta"))
-                    {
-                        NetworkReader.ReadMetatool(Reader.ReadFile(path), out matrix, out rev, out metabolite_names, out reaction_names);
-                        name = path.Substring(0, path.IndexOf(".meta"));
-                    }
-                    else
-                    {
-                        name = path;
-                        NetworkReader.ReadDir(name, separator, out matrix, out rev, out metabolite_names, out reaction_names, out forbidden);
-                    }
-                    Writer.WriteFile(NetworkReader.NetworkToDot(matrix, rev, metabolite_names, reaction_names), name + "_network.dot");
-                    Writer.WriteFile(NetworkReader.NetworkToConnections(matrix, rev, metabolite_names, reaction_names), name + "_connections.dot");
-                    Writer.WriteFile(NetworkReader.NetworkToMatlab(matrix, rev), name + ".m");
-
-                    model = name.Substring(name.LastIndexOf('\\') + 1);
-
-                    //bool[] trans = FindTrans(matrix, rev);
-
-                    // Calculations.
-
-                    LinkedList<String> lines = new LinkedList<string>();
-                    {
-                        FCA fca = null;
-                        method = fc ? "fc" : "all";
-                        for (int t = 0; t < n_simulations; ++t)
-                        {
-
-
-                            try
-                            {
-                                fca = new FCA(matrix, rev, forbidden, max_value, tolerance, fc, do_efca, !no_output);
-
-
-                                fca.Results(model, method, res, details);
-                                //Writer.WriteFile(details, String.Format("{0}_details_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
-                                Writer.WriteFile(res, String.Format("{0}_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
-                            }
-                            catch (CalculationException e)
-                            {
-                                Console.WriteLine("There's happened an error during the calculation. Repeating the simulation.");
-                                --t;
-                            }
-                        }
-
-                        // save results of last simulation
-
-                        String suffix = (fc ? "_fc" : "_all");
-
-                        lines.Clear();
-                        bool[] max = fca.max;
-                        int n = fca.n;
-                        for (int r = 0; r < n; ++r)
-                            if (!max[r])
-                                if (use_names)
-                                    lines.AddLast(reaction_names[r]);
-                                else
-                                    lines.AddLast(r.ToString());
-                        Writer.WriteFile(lines, name + "\\blocked" + suffix + ".txt");
-
-                        lines.Clear();
-                        for (int r = 0; r < n; ++r)
-                            if (max[r])
-                                for (int s = 0; s < n; ++s)
-                                    if (max[s] && !fca.coupling.Max(r)[s] && r != s)
-                                        if (use_names)
-                                            lines.AddLast(String.Format("{0} -> {1}", reaction_names[r], reaction_names[s]));
-                                        else
-                                            lines.AddLast(String.Format("{0} -> {1}", r, s));
-                        Writer.WriteFile(lines, name + "\\coupled" + suffix + ".txt");
-
-                        Writer.WriteFile(fca.coupling.ToDot(reaction_names), name + "\\coupled" + suffix + ".dot");
-
-                        if (do_efca)
-                            Writer.WriteFile(fca.efca.ToCSV(separator, reaction_names), name + "\\efca" + suffix + ".txt");
-
-                        Writer.WriteFile(res, String.Format("{0}_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
-                    }
-                }
-
-                // Finish stats.
-                res.AddLast("\\bottomrule");
-                res.AddLast("\\end{tabular}");
-                res.AddLast("\\end{table}");
-                Writer.WriteFile(res, String.Format("{0}_{1}_{2}-{3}-{4}-{5}-{6}.log", path2models.Substring(0, path2models.Length - 4), fc ? "fc" : "all", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute));
-
-            }
-            Console.WriteLine("Done. [press Enter]");
-            Console.ReadLine();
-        }
     }
 
 }
